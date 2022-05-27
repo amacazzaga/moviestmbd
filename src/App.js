7f2d5486e14a0371b880238c4bcc632a83d9ac2a
unknown <ag.macazz@gmail.com>
2022-05-27 16:22:31 -0300

    get data from server with axios

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import Header from "./Header/Header";
 import Body from "./Body/Body";
+import axios from "axios";
 import "./App.css";
 
 function App() {
@@ -8,15 +9,17 @@ function App() {
   const popularMovies = "discover/movie?sort_by=popularity.desc";
   const apiKey = "&api_key=0f0c22bee45b529c07d02b1f2dc14e01";
   const url = baseUrl + popularMovies + apiKey;
-  console.log(url)
   const [state, setState] = useState();
   useEffect(() => {
     getMoviesBy(url);
   }, []);
   const getMoviesBy = async (url) => {
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log(data);
+    axios
+      .get(url)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((e) => {});
   };
   return (
     <div>
