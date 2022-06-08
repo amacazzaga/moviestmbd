204ccdacf98cc840a1cedab5b5981431b6bcee19
unknown <ag.macazz@gmail.com>
2022-06-08 11:28:11 -0300

    d-flex class on navbar

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -4,26 +4,28 @@ import InputSearch from "./InputSearch";
 import HamburgerMenu from "./HamburgerMenu";
 import { useState } from "react";
 const Header = () => {
-  const [collapse,setCollapse]=useState("collapse")
+  const [collapse, setCollapse] = useState("collapse");
   return (
     <header className="container-xxl mt-15 ">
-      <nav class=" navbar-expand-sm ">
-        <div class="container-fluid">
-          <HamburgerMenu onClick={()=>{
-            if(collapse ==="collapse"){
-              setCollapse("collapse show")
-            }else{
-              setCollapse("collapse")
-            }
-          }} className="navbar-toggler" aria-expanded="true" />
+      <nav class=" navbar-expand-sm  ">
+        <div class="container-fluid d-flex">
+          <HamburgerMenu
+            onClick={() => {
+              if (collapse === "collapse") {
+                setCollapse("collapse show");
+              } else {
+                setCollapse("collapse");
+              }
+            }}
+            className="navbar-toggler"
+            aria-expanded="true"
+          />
 
-          <div class={`collapse navbar-${collapse}`}>
-            {/*here class show*/}
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <div class={`collapse navbar-${collapse}  justify-content-center`}>
+            <ul class="navbar-nav    ">
               <li class="nav-item dropdown">
                 <a
                   class="nav-link "
-                  id="navbarDropdown"
                   role="button"
                   data-bs-toggle="dropdown"
                   aria-expanded="false"
