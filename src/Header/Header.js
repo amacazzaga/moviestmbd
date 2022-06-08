52bf95ea3a36bad695db90ed8bce5fe473b66639
unknown <ag.macazz@gmail.com>
2022-06-08 12:29:09 -0300

    list group horizontal, fine

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -21,19 +21,13 @@ const Header = () => {
             aria-expanded="true"
           />
 
-          <div class={`collapse navbar-${collapse}  justify-content-center`}>
-            <ul class="navbar-nav    ">
-              <li class="nav-item dropdown">
-                <a
-                  class="nav-link "
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  <ButtonHeader text={"Genre"} />
-                  <ButtonHeader text={"Movies"} />
-                  <ButtonHeader text={"On TV"} />
-                </a>
+          <div class={`collapse navbar-${collapse}  justify-content-around`}>
+            <ul>
+              <li class=" list-group list-group-horizontal ">
+                <ButtonHeader text={"Genre"} />
+                <ButtonHeader text={"Movies"} />
+                <ButtonHeader text={"On TV"} />
+
                 <ul class="dropdown-menu" aria-labelledby="navbarDropdown"></ul>
               </li>
             </ul>
