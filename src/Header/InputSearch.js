2f1060680ae9953dabd292b30a66fb4124433a7d
unknown <ag.macazz@gmail.com>
2022-06-28 23:14:00 -0300

    search button working, almost fine

--- src/Header/InputSearch.js
+++ src/Header/InputSearch.js
@@ -1,12 +1,13 @@
 import React from "react";
-import { useEffect } from "react";
+import { useEffect,useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 
 const InputSearch = ({ getMoviesByGenre }) => {
+  const [inputvalue,setInputValue]=useState()
   const baseUrl = "https://api.themoviedb.org/3/search/movie";
   const apiKey = "api_key=0f0c22bee45b529c07d02b1f2dc14e01";
-  const query = "query=avengers";
+  const query = `query=${inputvalue}`;
   const urlInput = baseUrl + "?" + apiKey + "&" + query;
   useEffect(() => {
     getMoviesByGenre(urlInput);
@@ -15,15 +16,18 @@ const InputSearch = ({ getMoviesByGenre }) => {
     <form class=" container-md input-search d-flex ">
       <input
         class="outline-secondary m-2 "
-        type="search"
-        placeholder="Your Movie..."
+        type="text"
+        placeholder="Your Movie..."   
+        id= "input"  
+        onChange={(e)=>{setInputValue(e.target.value)}}
       ></input>
       <Link to={`/search/`}>
-        <button class="btn-outline-secondary m-2" type="submit">
+        <button onClick={()=>{getMoviesByGenre(urlInput)}} class="btn-outline-secondary m-2" type="submit">
           Search
         </button>
       </Link>
     </form>
+    
   );
 };
 
