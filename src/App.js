ef1af3b7b38f94c7fc42e27634b958a8caecdc7d
unknown <ag.macazz@gmail.com>
2022-07-01 20:29:47 -0300

    televisionitemhome as component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import Header from "./Header/Header";
-import TelevisionItem from "./Body/TelevisionItem";
+import TelevisionItemHome from "./Body/TelevisionItemHome";
 import TelevisionItemTelevision from "./Body/TelevisionItemTelevision";
 import MovieItemGenre from "./Body/MovieItemGenre";
 import MovieItemMovie from "./Body/MovieItemMovie";
@@ -14,10 +14,10 @@ const baseUrl = "https://api.themoviedb.org/3/discover";
 const baseUrlTv = "https://api.themoviedb.org/3/discover/tv?";
 const popularMovies = "/movie?sort_by=popularity.desc";
 const apiKey = "api_key=0f0c22bee45b529c07d02b1f2dc14e01";
+const imgUrlTv = "https://image.tmdb.org/t/p/original";
 const popularOnTv =
   "&language=en-US&sort_by=popularity.desc&page=1&timezone=America%2FNew_York&include_null_first_air_dates=false&with_watch_monetization_types=flatrate&with_status=0&with_type=0";
 const urlTv = baseUrlTv + apiKey + popularOnTv;
-const imgUrlTv = "https://image.tmdb.org/t/p/original";
 const url = baseUrl + popularMovies + "&" + apiKey;
 ////////////////////////////////////////////////////////////////////
 function App() {
@@ -138,18 +138,7 @@ function App() {
                           <nav className="navbar">
                             <ul className="p-1">
                               <li>
-                                {populartelevision
-                                  ? populartelevision.map((t) => (
-                                      <div className="mt-5">
-                                        <TelevisionItem
-                                          key={t.id}
-                                          name={t.name}
-                                          image={imgUrlTv + t.poster_path}
-                                          overview={t.overview}
-                                        />
-                                      </div>
-                                    ))
-                                  : "Loanding..."}
+                               <TelevisionItemHome populartelevision={populartelevision} loading={loading}/>
                               </li>
                             </ul>
                           </nav>
---
+++ src/Body/TelevisionItemHome.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import TelevisionItem from './TelevisionItem';
+const imgUrl = "https://image.tmdb.org/t/p/w780";
+const TelevisionItemHome = ({populartelevision,loading}) => {
+    return loading!=true ? (
+        populartelevision.map((m) => (
+          <div className="mt-5  ">
+            <TelevisionItem
+              key={m.id}
+              id={m.id}
+              title={m.title}
+              image={imgUrl + m.poster_path}
+              overview={m.overview}
+            />
+          </div>
+        ))
+      ) : (
+        <div className="spinner-border text-primary" role="status">
+          <span class="visually-hidden">Loading...</span>
+        </div>
+      );
+}
+
+export default TelevisionItemHome
