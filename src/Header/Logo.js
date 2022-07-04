93025b699a254c47d115d2c1f2aa8d5289bf173c
unknown <ag.macazz@gmail.com>
2022-07-04 12:30:08 -0300

    padding button on logo

--- src/Header/Logo.js
+++ src/Header/Logo.js
@@ -6,7 +6,7 @@ const Logo = () => {
   return (
     <Link  to={"/"}>
         <button
-        class=" btn-outline-secondary tmbd d-none d-lg-block"
+        class=" btn-outline-secondary tmbd d-none d-lg-block p-0 pb-3"
         type="button"
       >
        TMBD
