b7cce36e1488d1764c317f351fbd86420728dead
unknown <ag.macazz@gmail.com>
2022-07-06 11:34:20 -0300

    overview button working fine

--- src/Body/MovieItem.js
+++ src/Body/MovieItem.js
@@ -1,12 +1,35 @@
+import { useState } from "react";
 const MovieItem = ({ title, overview, image }) => {
+  const [dropdown, setDropdown] = useState("dropdown-menu");
   return (
     <div className="card bg-transparent">
-    <img src={image} className="card-img-top img-fluid" alt="..."></img>
-    <div className="card-body">
-      <h5 className="card-title">{title}</h5>
-      <p className="card-text">{overview}</p>
+      <img src={image} className="card-img-top img-fluid" alt="..."></img>
+      <div className="card-body">
+        <button
+          onClick={() => {
+            if (dropdown === "dropdown-menu") {
+              setDropdown("dropdown-menu show");
+            } else {
+              setDropdown("dropdown-menu");
+            }
+          }}
+          class="btn btn-secondary dropdown-toggle"
+          type="button"
+          id="dropdownMenuClickableOutside"
+          data-bs-toggle="dropdown"
+          data-bs-auto-close="inside"
+          aria-expanded="false"
+        >
+          Overview
+        </button>
+        <ul
+          class={dropdown}
+          aria-labelledby="dropdownMenuClickableOutside"
+        >
+          <p className="card-text">{overview}</p>
+        </ul>
+      </div>
     </div>
-  </div>
   );
 };
 
