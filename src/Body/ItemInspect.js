dc734b1466d005ae098a7c735f892a2853cac5bb
unknown <ag.macazz@gmail.com>
2022-07-13 16:35:05 -0300

    two columns on inspectedmovie

--- src/Body/ItemInspect.js
+++ src/Body/ItemInspect.js
@@ -4,13 +4,13 @@ const ItemInspect = ({ vote, image }) => {
   return (
     <div className="container-sm  px-1 pt-3 pb-3">
       <h1>{vote}</h1>
-      <div className="row">
-        <div className="col-sm-9 px-1">
+      <div className="row d-flex">
+        <div className="col-sm-12 col-lg-6 px-1">
           <div className="container px-1">
             <img className="img-fluid" src={image}></img>
           </div>
         </div>
-        <div className="col-sm-3">
+        <div className="col-lg-6">
           <div>eso</div>
           <div>eso</div>
           <div>eso</div>
